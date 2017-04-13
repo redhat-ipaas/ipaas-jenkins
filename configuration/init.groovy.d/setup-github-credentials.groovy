b3f37695ade0c48cce6145a24b4e9f1a08cfd310
Ioannis Canellos <iocanel@gmail.com>
2017-04-13 11:50:29 +0300

    Add another missing package.

--- configuration/init.groovy.d/setup-github-credentials.groovy
+++ configuration/init.groovy.d/setup-github-credentials.groovy
@@ -1,11 +1,16 @@
 import jenkins.model.*
+
 import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.common.*
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+
+import hudson.util.*;
 import hudson.plugins.sshslaves.*;
+
 import org.jenkinsci.plugins.plaincredentials.impl.*;
+
 
 domain = Domain.global()
 store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
