2dcdaa0b31e8701a3f8235f72499eb5eaaeb1e45
Ioannis Canellos <iocanel@gmail.com>
2017-11-02 15:43:10 +0200

    chore: update jobs from running environment.

--- bin/update-jobs-from-pod.sh
+++ bin/update-jobs-from-pod.sh
@@ -2,8 +2,18 @@
 
 POD=$1
 
-if [ -z "$POD" ];then
-	echo "Usage: update-jobs-from-pod.sh <pod name>"
-	exit -1
+JENKINS_HOME=$PWD
+
+if [ ! -d $JENKINS_HOME/configuration/jobs ] && [ -d $JENKINS_HOME/../configuration/jobs ]; then
+    JENKINS_HOME=$PWD/..
 fi
-for f in `kubectl exec $POD ls /var/lib/jenkins/jobs`;do mkdir -p configuration/jobs/$f && kubectl cp $POD:/var/lib/jenkins/jobs/$f/config.xml configuration/jobs/$f/config.xml;done
+
+
+for f in `kubectl exec $POD ls /var/lib/jenkins/jobs`; do
+
+    if [ ! -d $JENKINS_HOME/configuration/jobs/$f ]; then
+        mkdir $JENKINS_HOME/configuration/jobs/$f
+    fi
+    kubectl cp $POD:/var/lib/jenkins/jobs/$f/config.xml $JENKINS_HOME/configuration/jobs/$f/config.xml;
+done
+
